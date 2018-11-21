5c59fdc1ac737e712335fd4a3050755b89b0eb80
ihipop <ihipop@gmail.com>
2018-11-21 13:38:56 +0800

    curl with info

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eu;
 rm -rf src *tmp *.phar;
 mkdir -p tmp/;
 cd tmp/;
-curl -sL https://github.com/wudi/swoole-ide-helper/archive/master.zip -o master.zip
+curl -L https://github.com/wudi/swoole-ide-helper/archive/master.zip -o master.zip
 unzip -o *.zip  'swoole-ide-helper-master/src/*' -d src
 mv src/*/src ../
 cd ..
